227b9a3edf1b4dd5dbc4cc3b736f563cef3e17b7
her <58970041+limherhuey@users.noreply.github.com>
2021-06-15 14:27:51 +0800

    updated template

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import ChatScreen from './screens/ChatScreen';
-
-// just a random homescreen in place
-function HomeScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Helloo</Text>
-      <Button title="Chat now!" onPress={() => { navigation.navigate("Chat"); }}></Button>
-    </View>
-  );
-}
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createStackNavigator();
 
@@ -27,15 +17,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff6c8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    margin: 10,
-  },
-});
